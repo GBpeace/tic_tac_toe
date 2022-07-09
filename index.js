fbd1a3c94f93bcb7e169419b4cf8553412b648fb
gaurav bhosale <bhosalegaurav12@gmail.com>
2022-07-09 18:09:12 +0530

    tictactoenew

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var flag=true;
+var user1=0,user2=0;
 var states=[1,0,1,0,1,0,0,1,0];
 var winnerDiv=document.querySelector("#winner");
+
 document.querySelector(".container").addEventListener('click',function(e){
    if(e.target.id)
     setVal(e.target);
 })
-
 
 function setVal(curDiv)
 {
@@ -17,19 +18,21 @@ function setVal(curDiv)
             curDiv.innerHTML='X';
             states[id]='X';
             flag=!flag;
+            checkWinner(!flag);
         }
-
+        
     }
     else
-    {
+ {
         if(states[id]==1 || states[id]==0)
         {
             curDiv.innerHTML='O';
             states[id]='O';
             flag=!flag;
+            checkWinner(!flag);
         }
     }
-    checkWinner(!flag);
+    
     
     
 }
@@ -46,20 +49,69 @@ function setDisable()
 }
 function checkWinner(type)
 {
-    if((states[0]==states[1] && states[1]==states[2]) ||(states[3]==states[4] && states[4]==states[5])||(states[6]==states[7] && states[7]==states[8]))
+    const winIndex=[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    for(let x=0;x<winIndex.length;x++)
     {
+        let [a,b,c]=winIndex[x];
+        if(states[a]==states[b] && states[b]==states[c])
+        {
+            winnerDiv.innerHTML=type?"Winner X ":"Winner O";
+            setWinner(winIndex[x]);
+            setDisable();
+            setCount(type);
+            break;
+        }
+    }
+
+    
+}
+
+function setWinner(index)
+{
+    for(let i=0;i<index.length;i++)
+    {
+        // document.getElementById(index[i]).style.backgroundColor="rgb(233, 181, 233)";
+    }
+}
+function gameReset()
+{
+    var choice=confirm("Are you Sure Want to Reset The Game ?")
+    if(choice)
+    {   
         
-        winnerDiv.innerHTML=type?"Winner X ":"winner O";
-        setDisable();
+        flag=true;
+        states=[1,0,1,0,1,0,0,1,0];
+        divCollection=document.querySelectorAll(".tictac DIV");
+        for(let i=0;i<divCollection.length;i++)
+        {
+            divCollection[i].innerHTML="";
+            // divCollection[i].style.backgroundColor="#ff8080";
+            winnerDiv.innerHTML=" ";
+        }   
     }
-    else if((states[0]==states[3] && states[3]==states[6]) ||(states[1]==states[4] && states[4]==states[7])||(states[2]==states[5] && states[5]==states[8]))
-    {
-        winnerDiv.innerHTML=type?"Winner X":"winner O";
-        setDisable();
+}
+
+function setCount(type)
+{
+    (type)?user1++:user2++;
+    document.querySelector("#user1").innerHTML=user1;
+    document.querySelector("#user2").innerHTML=user2;
+}
+function restart(){
+    var choice=confirm("Are you Sure Want to Restart The Game ?")
+    if(choice)
+    {   
+        location.reload(); 
     }
-    else if((states[0]==states[4] && states[4]==states[8]) ||(states[2]==states[4] && states[4]==states[6]))
-    {
-        winnerDiv.innerHTML=type?"Winner X":"winner O";
-        setDisable();
-    }
+
 }
